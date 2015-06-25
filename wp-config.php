ae90c8cb39ee1645bee4ca9f8425f90ae4192833
Aurélien Debord <contact@aureliendebord.com>
2015-06-25 17:12:17 +0200

    Optimize WP db

---
+++ wp-config.php
@@ -0,0 +1,10 @@
+<?php
+//* Do NOT include the opening php tag shown above. Copy the code shown below.
+
+//* Set the home page and the url of the website
+define('WP_HOME', 'http://www.yoursite.com');
+define('WP_SITEURL', 'http://www.yoursite.com');
+
+//* Serve Static Content from a Cookie Free Domain
+define('COOKIE_DOMAIN', 'www.yoursite.com');
+
